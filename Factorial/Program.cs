04309ecd6ffc96c0a6ceb6d0d93bd1b96750a004
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-09 18:55:07 +0500

    Factorial 2.0

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,7 +1,9 @@
-﻿int Factorial(int n)
+﻿double Factorial(int n)
 {
     if(n == 1) return 1;
     else return n * Factorial(n-1);
 }
-
-Console.WriteLine(Factorial(5));
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
